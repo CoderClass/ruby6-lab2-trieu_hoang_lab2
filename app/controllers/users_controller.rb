c56194bc177cc3f926109d48775c4be4ccb0ddb8
phuonghoang713 <phuonghoang713@gmail.com>
2017-06-21 19:51:49 +0700

    setup and implement signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
 
+  def create
+      @user = User.new(user_params) # fill this out
+      if @user.save
+        flash[:success] = "Login success #{@user.name}"
+        session[:user_id] = @user.id
+        redirect_to users_path # create user, let user in and redirect to /users
+      else
+        flash[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+        redirect_to root_path
+      end
+  end
+
   def new
     @user = User.new
   end
@@ -7,4 +19,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
